17d17a8b15c2ffea982699591d15264dcc17c99b
Benjamin Chris <loli@lurenjia.in>
2020-03-31 14:22:17 +0800

    【Perf】后台布局样式改进

--- src/app/Views/layout/user_home.php
+++ src/app/Views/layout/user_home.php
@@ -19,6 +19,7 @@
         font-size: 24px;
         color: rgba(255,255,255,.7);
         transition: color 0.2s ease-in-out;
+        font-weight: 200;
     }
 
     #header .layui-logo a:hover {
@@ -47,8 +48,8 @@
 
     #sidebar .layui-nav .opml-title {
         display: flex;
-        padding-right: 10px;
-        padding-left: 5px;
+        padding-right: 12px;
+        padding-left: 8px;
     }
 
     #sidebar .layui-nav .opml-title span {
@@ -81,17 +82,24 @@
     /* 左侧边栏展宽 */
     .layui-layout-admin .layui-side,
     .layui-layout-admin .layui-side-scroll,
-    .layui-layout-admin .layui-side-scroll ul {
+    .layui-layout-admin .layui-side-scroll ul,
+    .layui-layout-admin .layui-logo {
         width: 260px !important;
     }
 
-    .layui-body {
+    .layui-body,
+    .layui-layout-left,
+    .layui-layout-admin .layui-footer {
         left: 260px !important;
     }
 
     /* 鼠标悬浮样式美化 */
     .layui-nav-tree .layui-nav-item > a:hover, .layui-nav-tree .layui-nav-item dd:hover {
         background-color: #159688 !important;
+    }
+
+    .layui-nav-item i {
+        margin-right: 4px;
     }
 
     span.layui-nav-bar {
@@ -101,21 +109,41 @@
     .required {
         color: red;
     }
+
+    #footer svg {
+        width: 16px;
+        display: inline-block;
+        vertical-align: middle;
+        opacity: 0.8;
+    }
+
+    #footer > span {
+        display: flex;
+        justify-content: space-between;
+        padding: 0 10px;
+    }
+
+    img#opmlhub-logo {
+        width: 30px;
+        display: inline-block;
+        vertical-align: sub;
+        margin-right: 8px;
+    }
 </style>
 <?= $this->endSection() ?>
 
 <?= $this->section("content") ?>
 <div id="app" class="layui-layout layui-layout-admin">
     <div id="header" class="layui-header">
-        <div class="layui-logo"><a href="/">OPMLHub</a></div>
+        <div class="layui-logo"><a href="/"><img id="opmlhub-logo" src="/assets/icon/icon.svg" alt="opmlhub" />OPMLHub</a></div>
         <!-- 头部区域（可配合layui已有的水平导航） -->
         <ul class="layui-nav layui-layout-left">
             <li class="layui-nav-item" id="tab-index"><a href="/user/home?module=index&page=index"><i class="layui-icon layui-icon-rss"></i>订阅配置</a></li>
             <li class="layui-nav-item" id="tab-settings"><a href="/user/home?module=settings&page=index"><i class="layui-icon layui-icon-slider"></i>高级设置</a></li>
         </ul>
         <ul class="layui-nav layui-layout-right">
-            <li class="layui-nav-item"><a href="/user/logout">修改密码</a></li>
-            <li class="layui-nav-item"><a href="/user/logout">退出</a></li>
+            <li class="layui-nav-item" id="tab-password"><a href="/user/home?module=password&page=index"><i class="layui-icon layui-icon-password"></i>修改密码</a></li>
+            <li class="layui-nav-item"><a href="/user/logout"><i class="layui-icon layui-icon-logout"></i>退出</a></li>
         </ul>
     </div>
     <div id="sidebar" class="layui-side layui-bg-black">
@@ -162,11 +190,17 @@
     </div>
 
     <div id="footer" class="layui-footer">
-        OPMLHub © 2020 | GitHub
+        <span>
+            <span class="left">OPMLHub © 2020</span>
+            <a class="right" target="_blank" href="https://github.com/LuRenJiasWorld/OPMLHub">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                GitHub
+            </a>
+        </span>
     </div>
 
     <div id="opml-share" style="display: none;">
-        <form class="layui-form">
+        <form class="layui-form" style="margin: 20px 10px 0;">
             <div class="layui-form-item">
                 <label class="layui-form-label">OPML链接</label>
                 <div class="layui-input-block">
@@ -195,6 +229,7 @@
         $(window).ready(function () {
             if (inUrlParameters("module", "index"))    $("#tab-index").addClass("layui-this");
             if (inUrlParameters("module", "settings")) $("#tab-settings").addClass("layui-this");
+            if (inUrlParameters("module", "password")) $("#tab-password").addClass("layui-this");
         });
 
         $(".opml-title span").click(function (event) {
@@ -208,7 +243,7 @@
             layer.open({
                 type: 0,
                 offset: 'auto',
-                area: '700px',
+                area: '660px',
                 content: $("#opml-share").html(),
                 btn: ["复制到剪贴板", "在新标签页打开"],
                 success: function() {
