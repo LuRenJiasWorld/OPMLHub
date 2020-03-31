4b7825191fc71cce758e55590935f6e60d9a69cb
Benjamin Chris <loli@lurenjia.in>
2020-03-31 15:20:02 +0800

    【Perf】优化高级设置表单样式

--- src/app/Views/user/settings.php
+++ src/app/Views/user/settings.php
@@ -6,11 +6,11 @@
 <hr />
 <form class="layui-form" style="margin-top: 45px;" action="/user/password" method="post">
 
-    <h2>拼音排序</h2>
+    <h2 style="margin: 10px; margin-top: 0;">拼音排序</h2>
 
     <div class="layui-form-item">
         <label class="layui-form-label">OPML列表</label>
-        <div class="layui-input-block">
+        <div class="layui-input-inline" style="width: 60px;">
             <input type="checkbox" name="switch" lay-skin="switch">
         </div>
         <div class="layui-form-mid layui-word-aux">拼音A-Z排序</div>
@@ -18,17 +18,18 @@
 
     <div class="layui-form-item">
         <label class="layui-form-label">RSS列表</label>
-        <div class="layui-input-block">
+        <div class="layui-input-inline" style="width: 60px;">
             <input type="checkbox" name="switch" lay-skin="switch">
         </div>
         <div class="layui-form-mid layui-word-aux">拼音A-Z排序</div>
     </div>
 
-    <h2>OPML设置</h2>
+
+    <h2 style="margin: 10px; margin-top: 40px;">OPML设置</h2>
 
     <div class="layui-form-item">
         <label class="layui-form-label">保留XML头</label>
-        <div class="layui-input-block">
+        <div class="layui-input-inline" style="width: 60px;">
             <input type="checkbox" name="switch" lay-skin="switch">
         </div>
         <div class="layui-form-mid layui-word-aux">兼容性需要，不确定请保持默认</div>
@@ -36,10 +37,10 @@
 
     <div class="layui-form-item">
         <label class="layui-form-label">包含Email</label>
-        <div class="layui-input-block">
+        <div class="layui-input-inline" style="width: 60px;">
             <input type="checkbox" name="switch" lay-skin="switch">
         </div>
-        <div class="layui-form-mid layui-word-aux">如果需要在OPML里隐藏Email地址，可以关闭此选项</div>
+        <div class="layui-form-mid layui-word-aux">关闭此选项以在OPML里隐藏Email地址</div>
     </div>
 
 
