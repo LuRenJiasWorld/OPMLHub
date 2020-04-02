f908eee14e519aaa8edefce380d82116253cf7ba
Benjamin Chris <loli@lurenjia.in>
2020-04-02 20:25:09 +0800

    【Feat】内建配置新增Loader

--- src/app/Config/App.php
+++ src/app/Config/App.php
@@ -1,26 +1,33 @@
 <?php namespace Config;
 
+use App\Controllers\Config;
 use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Base Site URL
-	|--------------------------------------------------------------------------
-	|
-	| URL to your CodeIgniter root. Typically this will be your base URL,
-	| WITH a trailing slash:
-	|
-	|	http://example.com/
-	|
-	| If this is not set then CodeIgniter will try guess the protocol, domain
-	| and path to your installation. However, you should always configure this
-	| explicitly and never rely on auto-guessing, especially in production
-	| environments.
-	|
-	*/
+    // 读取应用配置
+    function __construct() {
+        $this->baseURL = Config::$SiteURL;
+        $this->proxyIPs = Config::$GatewayIP;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Site URL
+    |--------------------------------------------------------------------------
+    |
+    | URL to your CodeIgniter root. Typically this will be your base URL,
+    | WITH a trailing slash:
+    |
+    |	http://example.com/
+    |
+    | If this is not set then CodeIgniter will try guess the protocol, domain
+    | and path to your installation. However, you should always configure this
+    | explicitly and never rely on auto-guessing, especially in production
+    | environments.
+    |
+    */
 	public $baseURL;
 
 	/*
