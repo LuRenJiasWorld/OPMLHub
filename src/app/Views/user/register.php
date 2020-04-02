eef6cb7df6a54acfe87d3c4a46fb5430202b3da7
Benjamin Chris <loli@lurenjia.in>
2020-04-02 14:21:32 +0800

    【Perf】增强注册页面用户体验

--- src/app/Views/user/register.php
+++ src/app/Views/user/register.php
@@ -2,31 +2,31 @@
 
 <?= $this->section("login_register_form") ?>
 
-<form class="layui-form" action="">
+<form class="layui-form" action="/user/register" method="post">
     <div class="layui-form-item form-input">
         <label class="layui-form-label">邮箱</label>
         <div class="layui-input-block">
-            <input type="email" name="email" required  lay-verify="required" placeholder="请输入电子邮箱地址" autocomplete="off" class="layui-input">
+            <input type="email" name="email" required lay-verify="required" placeholder="请输入电子邮箱地址" autocomplete="off" class="layui-input">
         </div>
     </div>
 
     <div class="layui-form-item form-input">
         <label class="layui-form-label">密码</label>
         <div class="layui-input-block">
-            <input type="password" name="password" required  lay-verify="required" autocomplete="off" class="layui-input">
+            <input type="password" name="password" minlength="6" maxlength="16" required lay-verify="required" autocomplete="off" class="layui-input">
         </div>
     </div>
 
     <div class="layui-form-item form-input">
         <label class="layui-form-label">确认密码</label>
         <div class="layui-input-block">
-            <input type="password" name="password-again" required  lay-verify="required" autocomplete="off" class="layui-input">
+            <input type="password" name="password_again" minlength="6" maxlength="16" required lay-verify="required" autocomplete="off" class="layui-input">
         </div>
     </div>
 
     <div class="layui-form-item">
         <div class="layui-input-block button-center">
-            <button type="reset" class="layui-btn layui-btn-fluid">注册</button>
+            <button type="submit" lay-submit class="layui-btn layui-btn-fluid">注册</button>
         </div>
     </div>
 
