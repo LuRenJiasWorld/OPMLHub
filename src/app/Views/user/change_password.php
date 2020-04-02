1f0025ce197194c00010549f585e716ecf0ceb42
Benjamin Chris <loli@lurenjia.in>
2020-04-02 14:27:38 +0800

    【Perf】增强密码重置页面用户体验

--- src/app/Views/user/change_password.php
+++ src/app/Views/user/change_password.php
@@ -4,7 +4,7 @@
 <?= $this->section("user_home_page") ?>
 <h1>修改密码</h1>
 <hr />
-<form class="layui-form" style="margin-top: 45px;" action="/user/password" method="post">
+<form class="layui-form" style="margin-top: 45px;" action="/user/update?type=password" method="post">
     <div class="layui-form-item">
         <label class="layui-form-label">旧密码<i class="required">*</i></label>
         <div class="layui-input-block">
@@ -15,14 +15,15 @@
     <div class="layui-form-item">
         <label class="layui-form-label">新密码<i class="required">*</i></label>
         <div class="layui-input-block">
-            <input type="password" name="new_password" required lay-verify="required" autocomplete="off" class="layui-input">
+            <input type="password" name="new_password" minlength="6" maxlength="16" required lay-verify="required" autocomplete="off" class="layui-input">
         </div>
     </div>
 
     <div class="layui-form-item">
         <label class="layui-form-label">确认密码<i class="required">*</i></label>
         <div class="layui-input-block">
-            <input type="password" name="new_password_again" required lay-verify="required" autocomplete="off" class="layui-input">
+            <input type="password" name="new_password_again" minlength="6" maxlength="16" required lay-verify="required" autocomplete="off" class="layui-input">
+            <div class="layui-form-mid layui-word-aux">新密码长度需在6~16字符间，且至少包含字母和数字</div>
         </div>
     </div>
 
